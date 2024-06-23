bb92ee2c5b5b94463243347e5a4dec97db42764b
João Pedro Zabarella <zabarellaj@gmail.com>
2024-06-23 18:38:21 -0300

    Arrumando erros e modificando a parte de experiencia
    Arrumei o erro da ultima letra apagada não estar aparecendo, retirei a section contact e mudei o nome para projetos.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const typingText = document.querySelector(".typing-text");
-    const words = ["Desenvolvedor Java", "Desenvolvedor de Software", "Analista de Software", "Desenvolvedor de Banco de Dados", "Desenvolvedor Python"];
+    const words = ["Desenvolvedor Java", "Desenvolvedor de Software", "Analista de Software", "Desenvolvedor Back-end", "Desenvolvedor Python"];
     let wordIndex = 0;
     let charIndex = 0;
     let isDeleting = false;
@@ -10,7 +10,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function type() {
         const currentWord = words[wordIndex];
-        let displayText = isDeleting ? currentWord.substring(0, charIndex--) : currentWord.substring(0, charIndex++);
+        if (isDeleting) {
+            charIndex--;
+        } else {
+            charIndex++;
+        }
+        
+        let displayText = currentWord.substring(0, charIndex);
         typingText.textContent = displayText;
 
         if (!isDeleting && charIndex === currentWord.length) {
@@ -27,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     type();
 
-    // Toggle para o menu responsivo
+    
     const menuIcon = document.querySelector(".fa-bars");
     const navbar = document.querySelector(".navbar");
 
@@ -35,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function() {
         navbar.classList.toggle("active");
     });
 
-    // Fechar o menu ao clicar em um link (responsividade)
+  
     const navLinks = document.querySelectorAll(".navbar a");
 
     navLinks.forEach(function(navLink) {
@@ -44,4 +50,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
-
